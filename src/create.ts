eb3331f9f2245b901530dc061a499fe8edc48b2c
Erazul Islam <erazul89@gmail.com>
2024-12-08 23:02:10 +0600

    added

--- src/create.ts
+++ src/create.ts
@@ -1,11 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
 const main = async () => {
-    
-    const getData = await prisma.post.findMany()
-    console.log(getData)
+    const createUser = await prisma.user.create({
+        data : {
+            userName : 'userName2',
+            email : "bahu@gmail2.com",
+            role : UserRole.user
+        }
+    })
+    console.log(createUser)
 }
 
 main()
